a1b22703422f83235e45b74c5c01b4be3c49c6a9
Rogsu <gonzalo.marolda@gmail.com>
2025-04-23 23:26:38 -0300

    Last updated

--- client/src/app/models/wheel.ts
+++ client/src/app/models/wheel.ts
@@ -7,13 +7,13 @@ export class Wheel {
     entries: Entry[]
     fatherWheelId?: string
     fatherEntryId?: string
-    createdAt: string
+    createdAt?: string
+    updatedAt?: string
 
     constructor() {
         this.id = crypto.randomUUID()
         this.name = "New Wheel"
         this.userId = 0
         this.entries = []
-        this.createdAt = ""
     }
 }
